0e058818842b31e71d6d32230238ccf3f9d77499
virendrak07 <virenkadam07@gmail.com>
2022-06-11 16:34:02 +0200

    home page functionality

--- client/src/app/modules/home-page/home-page.component.ts
+++ client/src/app/modules/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewportScroller } from "@angular/common";
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private scroller: ViewportScroller) { 
+// for scroll page to top
+window.scrollTo({
+  top: 0,
+});
 
-  ngOnInit(): void {
-  }
-
+// for AOS animation
+AOS.init({
+  delay: 0,
+  // once: true,
+  duration: 500, // values from 0 to 3000, with step 50ms
+  easing: 'ease-in-out', // default easing for AOS animations
+});
+}
+goDown() {
+  this.scroller.scrollToAnchor("targetHotel");
+};
+ngOnInit(): void {}
 }
