64e414b151b6de3d6d654c044011ec2a92a367f7
virendrak07 <virenkadam07@gmail.com>
2022-06-12 17:38:56 +0200

    User login page CRUD operation

--- client/src/app/modules/user-login-page/user-login-page.component.ts
+++ client/src/app/modules/user-login-page/user-login-page.component.ts
@@ -1,4 +1,10 @@
+import { ViewportScroller } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import * as AOS from 'aos';
+import { CommonHttpService } from 'src/app/services/common-http.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-user-login-page',
@@ -6,10 +12,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-login-page.component.scss']
 })
 export class UserLoginPageComponent implements OnInit {
+  loginForm = new FormGroup({
+    userEmail: new FormControl('', [Validators.required, Validators.email]),
+    // email_id: new FormControl('', Validators.required),
+    userPassword: new FormControl('', [Validators.required])
+  });
+  constructor(private scroller: ViewportScroller, private router: Router, private commonService: CommonHttpService) {
+    // for scroll page to top
+    window.scrollTo({
+      top: 0,
+    });
+    //scroll
 
-  constructor() { }
-
-  ngOnInit(): void {
+    // for AOS animation
+    AOS.init({
+      delay: 0,
+      // once: true,
+      duration: 500, // values from 0 to 3000, with step 50ms
+      easing: 'ease-in-out', // default easing for AOS animations
+    });
   }
+  goDown() {
+    this.scroller.scrollToAnchor("targetHotel");
+  };
+
+  /**
+* This function represent User Login
+* @param {*} req 
+* @param {*} res 
+* @author Virendra Kadam
+*/
+  login() {
+    if (this.loginForm.valid) {
+      this.commonService.postSecure(environment.userLogin, this.loginForm.value).subscribe(res => {
+        if (!res.error) {
+          localStorage.setItem('userId', res.userId);
+          this.router.navigate(['bidding/']);
+        } else {
+          alert('Invalid Credentials!!!')
+        }
+      })
+    } else {
+      alert('Please insert all fields!!!')
+    }
+  }
+  
+  ngOnInit(): void { }
+
 
 }
