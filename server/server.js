41fbeb43fcb36bf4e427323e0b7f958086c55375
projectssrh <projects_srh@protonmail.com>
2022-06-14 02:24:51 +0200

    path added in server.js

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ let auth = require('./app/route/auth/auth.route')
 let user_details = require('./app/route/user_details/user_details.route')
 let hotel_details = require('./app/route/hotel_details/hotel_details.route')
 let city_master =  require('./app/route/city_master/city_master.route')
+let user_bid_details = require('./app/route/user_bid_details/user_bid_details.route')
 
 let sqlinjection = require('sql-injection')
 
@@ -20,6 +21,7 @@ app.use('/auth', auth)
 app.use('/userDetails', user_details)
 app.use('/hotelDetails', hotel_details)
 app.use('/cities', city_master)
+app.use('/userBid',user_bid_details)
 
 
 // app.use('/syscon', syscon)
